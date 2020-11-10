a199a9beab7ce9d3874908fc1fd8c636c1a70fac
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-10 16:27:35 -0700

    added accordion component which presents the historical data

---
+++ components/Accordion.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { List } from 'react-native-paper';
+
+export default function Accordion({title, data, month}: any) { 
+		let keys = Object.keys(data)
+
+		let filteredByMonth = keys
+		.filter(key =>
+			new Date(key).getMonth() == month)
+
+    return (
+        <List.Section>
+            <List.Accordion
+            title={title}
+            >
+							{filteredByMonth.map((date,key) => {
+								return (
+									<List.Item
+										key={key}
+										title={data[date]['activity']}
+										description={["Duration: ", data[date]['duration'],"\n",
+										  "Emotion: ", data[date]['emotion'], "\n",]}
+										
+										/> 
+								)
+							})}
+            </List.Accordion>
+        </List.Section>
+    )
+}
