3665d2b4ec8a7b191e390ab26a9d24e590e1b333
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-10 16:26:56 -0700

    updated home screen to have navigation/ be a landing page

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,94 +1,38 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { PickerIOS } from "@react-native-picker/picker";
-import React, { useState } from "react";
-import { Button, SafeAreaView, StyleSheet, Text } from "react-native";
+import React from "react";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import { AuthNavProps } from "../src/AuthParamList";
 
-const ACTIVITIES= [ "", 'Running', 'Swimming', 'Sports', 'Workout', 'Yoga', 'Meditation']
-const DURATIONS = [ "", '10 min', '15 min', '30 min', '45 min', '60 min']
-const EMOTIONS = [ "", 'Great!', 'Good', 'Challenging', 'Uplifting', 'Energizing']
-
-// gloval state store. Redux is annoying. Don't use this unless your livelihood depends on you learning this
-// useContext for quick global state stuff
-// And honestly, prop threading isn't that bad, and I'd just default to this, as long as you can keep it realllllly simple.
-
-export default function Home({ user }: any) {
-  const [ activity, setActivity ] = useState("");
-  const [ duration, setDuration ] = useState("");
-  const [ emotion, setEmotion ] = useState("");
-
-  function handleOnChange() {
-    let childData = {activity, duration, emotion}
-    let date: Date = new Date();
-    let stringDate = date.toString();
-    user[stringDate] = childData
-    saveUserInfo;
+function Home({navigation}: AuthNavProps<'Home'>) {
+    return(
+      <SafeAreaView>
+        <View style={styles.container}>
+        <Button
+        onPress={() =>{
+          navigation.navigate("Activity")
+        }}
+        >
+          <Text>Track an Activity</Text>
+        </Button>
+        <Button
+          onPress={() => {
+            navigation.navigate("History")
+          }}
+        >
+          <Text>See previous Activites</Text>
+        </Button>
+      </View>
+      </SafeAreaView>
+    )
   }
 
-
-  const saveUserInfo = async () => {
-    try {
-      await AsyncStorage.setItem('user', JSON.stringify(user))
-    } catch (e) {
-      console.log(e)
+  const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 15,
+      marginVertical: 300,
+      maxWidth: 600, //web view
+      justifyContent: 'center',
     }
-  };
+  });
+export default Home;
 
-  return (
-      <SafeAreaView>
-          <Text style={{alignSelf:"center",fontSize:30, marginTop:20}}>Select an activity</Text>
-          <PickerIOS
-          selectedValue={activity}
-          onValueChange={(value: any) => {
-              setActivity(value);
-          }}>
-          {ACTIVITIES.map((value) => (
-              <PickerIOS.Item
-              key={value}
-              value={value}
-              label={value}
-              />
-          ))}
-          </PickerIOS>
-          <Text style={styles.titles}>How long was the activity?</Text>
-          <PickerIOS
-          selectedValue={duration}
-          onValueChange={(value: any) => {
-              setDuration(value);
-          }}>
-          {DURATIONS.map((value) => (
-              <PickerIOS.Item
-              key={value}
-              value={value}
-              label={value}
-              />
-          ))}
-          </PickerIOS>
-          <Text style={styles.titles}>How did it feel?</Text>
-          <PickerIOS
-          selectedValue={emotion}
-          onValueChange={(value: any) => {
-              setEmotion(value);
-          }}>
-          {EMOTIONS.map((value) => (
-              <PickerIOS.Item
-              key={value}
-              value={value}
-              label={value}
-              />
-          ))}
-          </PickerIOS>
-          <Button
-          title="Submit"
-          onPress={handleOnChange}
-          />
-      </SafeAreaView>
-  )
-}
-
-
-const styles = StyleSheet.create!({
-    titles: {
-        alignSelf: 'center',
-        fontSize: 30,
-    }
-})
