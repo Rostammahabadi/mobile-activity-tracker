1071e76bda1834e30f8cb6a8b8861c664f9c70a1
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-09 21:37:09 -0700

    set user state, async storage on load, async storage setItem after button click on the child Home component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
+  const [user, setUser] = React.useState(Object);
+
+  const getMyObject = async () => {
+    const previousUser = await AsyncStorage.getItem('user');
+    if(previousUser){
+      setUser(JSON.parse(previousUser))
+    } else {
+      try {
+        await AsyncStorage.setItem('user', JSON.stringify({}))
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  };
+
+  const saveUserInfo = async (user: any) => {
+    try {
+      await AsyncStorage.setItem('user', JSON.stringify(user))
+    } catch (e) {
+      console.log(e)
+    }
+  };
+
+  useEffect(() => {
+    getMyObject();
+  },[]);
+
+  const handleOnChange = (childData: Object) => {
+    let date: Date = new Date();
+    let stringDate = date.toString();
+    user[stringDate] = childData
+    saveUserInfo(user);
+  }
+
   return (
-    <Home/>
+    <Home
+      onPress={handleOnChange}
+    />
   );
 }
 
