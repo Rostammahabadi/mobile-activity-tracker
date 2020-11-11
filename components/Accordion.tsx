1cf0fb52bc7bb11ccf77753e6e1ac2a9d798e8f6
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-10 23:24:32 -0700

    used unique key and added check for when the user doesn't choose an activity/duration/emotion

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import { List } from 'react-native-paper';
 
 export default function Accordion({title, data, month}: any) { 
-		let keys = Object.keys(data)
+  let keys = Object.keys(data)
 
-		let filteredByMonth = keys
-		.filter(key =>
-			new Date(key).getMonth() == month)
-
-    return (
-        <List.Section>
-            <List.Accordion
-            title={title}
-            >
-							{filteredByMonth.map((date,key) => {
-								return (
-									<List.Item
-										key={key}
-										title={data[date]['activity']}
-										description={["Duration: ", data[date]['duration'],"\n",
-										  "Emotion: ", data[date]['emotion'], "\n",]}
-										
-										/> 
-								)
-							})}
-            </List.Accordion>
-        </List.Section>
-    )
+  let filteredByMonth = keys
+  .filter(key =>
+      new Date(key).getMonth() == month)
+    
+  return (
+      <List.Section>
+          <List.Accordion
+          title={title}
+          >
+                          {filteredByMonth.map((date, num) => {
+                              return (
+                                  <List.Item
+                                      key={num}
+                                      title={data[date]['activity']}
+                                      description={["Duration: ", data[date]['duration'],"\n",
+                                        "Emotion: ", data[date]['emotion'], "\n",]}
+                                      
+                                      /> 
+                              )
+                          })}
+          </List.Accordion>
+      </List.Section>
+  )
 }
